1a8b3cb509c39ac13117473536b57c463b5f3d6a
MrCrabs69 <73902607+MrCrabs69@users.noreply.github.com>
2021-12-25 10:55:45 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import DrawerNavigator from './navigation/DrawerNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
---
+++ screens/CreatePost.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class CreatePost extends Component{
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>
+                  Create Post Screen
+                </Text>
+            </View>
+        )
+    }
+}
